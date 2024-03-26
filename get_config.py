afe013ded64a4e629576c078f8b4c4750ac18c10
Nacer Benyoub <nacer.kh.ben@gmail.com>
2024-03-26 16:23:10 +0100

    Update data path and reference atlas location in get_config.py

--- get_config.py
+++ get_config.py
@@ -1,14 +1,13 @@
 from pathlib import Path
+import os
 
 def get_config_dict():
     config = {}
-    #config["data_path"] = Path(r"/media/pedro/Data/PhD/UoE/Data/ADNI") / "raw_data"
-    config["data_path"] = Path(r"/home/s2086085/idcom_data/Brain/ADNI1_raw/") 
+    config["data_path"] = Path(r"./raw_data/") 
     config["re_process"] = False
     
     resolution_mm = 2
-    #config["reference_atlas_location"] = Path(f'/usr/local/fsl/data/standard/MNI152_T1_{resolution_mm}mm_brain.nii.gz')
-    config["reference_atlas_location"] = Path(f'/home/s2086085/RDS/fsl/data/standard/MNI152_T1_{resolution_mm}mm_brain.nii.gz')
+    config["reference_atlas_location"] = Path(f'{os.environ["FSLDIR"]}/data/standard/MNI152_T1_{resolution_mm}mm_brain.nii.gz')
     config["axial_size"] = 90
     config["save_2d"] = True
     return config
